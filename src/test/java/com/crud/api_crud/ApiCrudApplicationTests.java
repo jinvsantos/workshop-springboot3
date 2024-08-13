154c3f51f5bc68e6a952e96df39854835e70a4a1
Jean Santos <jeanvictor.san@gmail.com>
2024-08-13 15:57:57 -0300

    project created

---
+++ src/test/java/com/crud/api_crud/ApiCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.api_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
